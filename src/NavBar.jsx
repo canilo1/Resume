a79b8e2a6610d55db500043f52029ea953219ef8
John Gutierrez <75353216+canilo1@users.noreply.github.com>
2025-03-01 20:20:09 -0800

    MAde background black

---
+++ src/NavBar.jsx
@@ -0,0 +1,18 @@
+const NavBar = () =>{
+    <>
+    <div id = "NavBar">
+    <h1 id = "JourneyBar">
+        My Journey
+    </h1>
+    <h1 id = "My Experience Bar">
+        My Experience
+    </h1>
+    <h1 id = "BlogBar">
+        Blog
+    </h1>
+    </div>
+    </>
+
+}
+
+export default NavBar
